a459c34fbf240038aee2427b32c5df2af919ce7f
Lbgraff <lbgraff92@gmail.com>
2024-09-04 15:51:58 -0500

    final push before deployment

--- Main/public/js/comment.js
+++ Main/public/js/comment.js
@@ -4,7 +4,7 @@ const newFormHandler = async (event) => {
     const commentText = document.querySelector('#comment-name').value.trim();
   
     if (commentText) {
-      const response = await fetch(`/api/comment`, {
+      const response = await fetch(`/api/blogpost/comment`, {
         method: 'POST',
         body: JSON.stringify({ commentText }),
         headers: {
@@ -16,22 +16,6 @@ const newFormHandler = async (event) => {
         document.location.replace('/profile');
       } else {
         alert('Failed to create comment');
-      }
-    }
-  };
-  
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/blogposts/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete comment');
       }
     }
   };
